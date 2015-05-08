9583524e0c12c6b3df6de4a0a185d4912b84fabe
rails <rails@rails.com>
2015-05-08 15:42:29 +0530

    Config deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
-#production.rb
-set :domain, "63.142.250.46"
+#production
+set :domain, "107.155.113.112"
 set :rails_env, 'production'
-set :gitrepo, "git@github.com:neeraji2it/ics.git"
-set :deploydir, "/var/www/ics.com/public_html"
-set :user, "neeraj"
+set :gitrepo, "git@github.com:neeraji2it/touchsecurecrafts.git"
+set :deploydir, "/var/www/touchsecurecrafts.com/public_html"
+set :user, "shivraj"
 set :use_sudo, false
