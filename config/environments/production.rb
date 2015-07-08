f3ddb5a4a2e0067d177335999bb0531b1541ac8d
rails <rails@rails.com>
2015-07-08 17:19:48 +0530

    disable caching, which is making trouble in production server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ ICS::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
@@ -83,7 +83,7 @@ config.i18n.fallbacks = true
     :authentication       => 'plain',
     :enable_starttls_auto => true
   }
-  
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
