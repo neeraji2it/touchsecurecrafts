b3ed6b5dfdb0e10899f6e827f79f1dff8d73b91f
rails bhagath <rails@gmail1.com>
2015-07-21 11:28:27 +0530

    mail sending testing in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,8 @@ config.i18n.fallbacks = true
     :authentication       => 'plain',
     :enable_starttls_auto => true
   }
-
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
-SITE = 'handicraftstudios.com'
+SITE = 'touchsecurecrafts.com'
